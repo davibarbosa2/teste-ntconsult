b1fd4b391fd6def7c0fe65c4818170805dfcd39a
Davi Rangel <davi.rangel@zooxsmart.com>
2024-08-07 12:11:56 -0300

    add icone de notificação no main-nav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,20 +1,21 @@
 <template>
   <nav class="flex items-center justify-between">
-    <router-link
-      to="/"
-      class="text-sm font-medium transition-colors"
-    >
-      <img
-        src="@/assets/Logotipo.png"
-        alt="Logotipo"
-        class="h-8 w-auto"
-      >
+    <router-link to="/" class="text-sm font-medium transition-colors">
+      <img src="@/assets/Logotipo.png" alt="Logotipo" class="h-8 w-auto" />
     </router-link>
 
-    <UserDropdown />
+    <div class="flex items-center gap-2">
+      <Button variant="ghost" size="icon" class="rounded-full">
+        <Bell />
+      </Button>
+
+      <UserDropdown />
+    </div>
   </nav>
 </template>
 
 <script lang="ts" setup>
+  import { Bell } from "lucide-vue-next";
   import UserDropdown from "./UserDropdown.vue";
+  import Button from "./ui/button/Button.vue";
 </script>
